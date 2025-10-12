41160b8d3725fe50a37ec75e5911ff3398b91ef4
Dimitri PUYET <dimitripuyet@gmail.com>
2025-10-12 16:09:20 +0200

    import google font

--- components/header.py
+++ components/header.py
@@ -1,7 +1,14 @@
 from nicegui import ui
 
 def header():
-    with ui.header().classes('bg-[#FDF7E9] flex items-center justify-center font-mono'):
+
+    ui.add_head_html('''
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet">
+''')
+
+    with ui.header().classes('bg-[#FDF7E9] flex items-center justify-center font-GochiHand').style('font-family: "Gochi Hand", cursive;'):
         with ui.link(target='/'):
-            ui.image(source="static/logo_pizza.png").classes('w-16')
-        ui.label("Dim Pizza Calculator").classes('text-2xl text-black flex-grow text-center')
+            ui.image(source="static/logo_pizza.png").classes('w-18')
+        ui.label("Dim Pizza Calculator").classes('text-4xl text-black flex-grow text-center')
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from nicegui import ui
 import pages.home, pages.protocole
+
+
+
+
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run(favicon='🍕', title="Dim Pizza Calculator", port=8000)
