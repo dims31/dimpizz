b0a58760092519ada674282507222f6539b02c4c
Dimitri PUYET <dimitripuyet@gmail.com>
2025-10-12 18:03:52 +0200

    push menu et petrissage

--- components/header.py
+++ components/header.py
@@ -8,7 +8,11 @@ def header():
 <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet">
 ''')
 
-    with ui.header().classes('bg-[#FDF7E9] flex items-center justify-center font-GochiHand').style('font-family: "Gochi Hand", cursive;'):
+    with ui.header().classes('bg-[#FDF7E9] flex items-center justify-center').style('font-family: "Gochi Hand", cursive;'):
         with ui.link(target='/'):
             ui.image(source="static/logo_pizza.png").classes('w-18')
         ui.label("Dim Pizza Calculator").classes('text-4xl text-black flex-grow text-center')
+        with ui.button(icon='menu').classes('bg-transparent text-black'):
+            with ui.menu() as menu:
+                ui.menu_item('Protocole', on_click= lambda: ui.navigate.to('/protocole'))
+                    
--- pages/protocole.py
+++ pages/protocole.py
@@ -4,4 +4,28 @@ from components.header import header
 @ui.page('/protocole')
 def page():
     header()
-    ui.label("Protocole de fabrication").classes('text-2xl text-black flex-grow text-center')
+    ui.colors(primary='#555')
+    with ui.stepper().props('vertical').classes('w-full') as stepper:
+        with ui.step('Pétrissage'):
+            ui.label("Ajouter dans l'eau à température ambiante la levure et le sel. Mélanger jusqu'à dissolution complète.")
+            ui.label("Ajouter la farine et l'huile. Mélanger jusqu'à obtenir une pâte homogène.")
+            ui.label("Pétrisser la pâte pendant 10 minutes.")
+            with ui.stepper_navigation():
+                ui.button('Suivant', on_click=stepper.next)
+        with ui.step('Pointage'):
+            ui.label("Laisser la pâte reposer pendant 3 heures à température ambiante dans un saladier couvert d'un film étirable.")
+            with ui.stepper_navigation():
+                ui.button('Suivant', on_click=stepper.next)
+                ui.button('Retour', on_click=stepper.previous).props('flat')
+        with ui.step('Boulage'):
+            ui.label('Faire des boules de pâte de la taille désirée (250g pour une pizza de 30cm).')
+            ui.label('Laisser reposer les boules de pâte à température ambiante ou température contrôlée (4°C) selon le protocole choisi.')
+            with ui.stepper_navigation():
+                ui.button('Suivant', on_click=stepper.next)
+                ui.button('Retour', on_click=stepper.previous).props('flat')
+        with ui.step('Cuisson'):
+            ui.label('Préchauffer le four à 400°C')
+            ui.label('Cuire les pizzas pendant 3 minutes ou jusqu\'à ce qu\'elles soient dorées.')
+            with ui.stepper_navigation():
+                ui.button('Terminé', on_click=lambda: ui.notify('Buon appetito!', type='positive')) 
+                ui.button('Retour', on_click=stepper.previous).props('flat')
